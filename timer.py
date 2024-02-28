c67f29e1113ec4bfc6fa28ff7de3f0372324d796
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2024-02-28 04:36:11 -0600

    Agregamos un timer al boton generar
    Dividimos nuestro trabajo, en por el momento 2 clases *timer* y *gui*:
    
    # Timer
    Aquí utilizamos hilos para manejar el reloj, usando como funcion principal
    **update_time**
    
    # MyGUI
    En este debera estar toda nuestra interfaz grafica, por lo que solo creamos
    la funcion *start_timer* la cual se comunica con la clase *Timer*

---
+++ timer.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import threading
+
+class Timer:
+    def __init__(self):
+        self.time_string = tk.StringVar()
+        self.running = False
+        self.seconds = 0
+        
+        
+
+    def update_time(self,window,canvas,clock):
+        if self.running:
+            self.seconds += 1
+            minutes = self.seconds // 60
+            seconds = self.seconds % 60
+            self.time_string.set(f"{minutes}:{seconds:02}")
+            canvas.itemconfig(clock, text=self.time_string.get())
+            window.after(1000, self.update_time,window,canvas,clock)
+    
+    def start(self, window, canvas, clock):
+        
+        self.running = True
+        threading.Thread(target=self.update_time,args=(window,canvas,clock)).start()
+
+    def stop(self, canvas, clock):
+        self.running = False
+        self.seconds = 0
